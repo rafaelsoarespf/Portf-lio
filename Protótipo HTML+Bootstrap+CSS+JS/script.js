3c77e66e2e23c40438cbade426b5388c0720c874
Rafael Soares de Paula FIlho <rafaelsoares.p.f@gmail.com>
2025-03-12 04:26:56 -0300

    adicionado animação de texto que escreve sozinho na sessão de apresentação

---
+++ Protótipo HTML+Bootstrap+CSS+JS/script.js
@@ -0,0 +1,46 @@
+//=======  MENSAGEM =========================================
+
+//frases que serão mostradas
+const arrayText = [
+    "Eu sou Rafael Soares de Paula Filho!",
+];
+
+//tempo que leva para escrever
+const writeTime = 100;
+
+//tempo que leva para apagar
+const removeTime = 1000;
+
+//pega o indice do array
+let indexSentence = 0;
+//poga o incice do character
+let indexChar = 0;
+
+const element = document.querySelector("#mensagem");
+console.log("TCL: element", element)
+
+function writeText() {
+    if(indexChar <= arrayText[indexSentence].length) {
+        element.textContent = arrayText[indexSentence].substring(0,indexChar);
+        indexChar++;
+        setTimeout(writeText, writeTime)
+    } else {
+        setTimeout(removeText,removeTime)
+    }
+}
+
+function removeText() {
+    if(indexChar >= 0){
+        element.textContent = arrayText[indexSentence].substring(0, indexChar)
+        indexChar--;
+        setTimeout(removeText,writeTime);
+    }else{
+        indexSentence++
+        if(indexSentence >= arrayText.length){
+            indexSentence = 0;
+        }
+        setTimeout(writeText,removeText);
+    }
+}
+
+writeText();
