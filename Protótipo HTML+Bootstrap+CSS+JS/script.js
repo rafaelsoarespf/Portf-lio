bd27e0d88e79a818748c79204dc975a2a4b3f6de
Rafael Soares de Paula FIlho <rafaelsoares.p.f@gmail.com>
2025-03-15 23:57:51 -0300

    criando dark mode

--- Protótipo HTML+Bootstrap+CSS+JS/script.js
+++ Protótipo HTML+Bootstrap+CSS+JS/script.js
@@ -44,3 +44,13 @@ function removeText() {
 }
 
 writeText();
+
+
+//darkmode
+
+const $html = document.querySelector('html')
+const $checkbox = document.querySelector('#switch')
+
+$checkbox.addEventListener('change', function(){
+    $html.classList.toggle('dark-mode')
+})
